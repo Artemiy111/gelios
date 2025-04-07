91b3733e258c466029e3a7daa298e44046fe435f
Artemiy111 <mrart111@mail.ru>
2025-04-07 14:13:54 +0300

    .

--- server/api/users/feedback.post.ts
+++ server/api/users/feedback.post.ts
@@ -2,20 +2,20 @@ import { authRepo } from '~~/server/repos/auth'
 import { userRepo } from '~~/server/repos/user'
 import { feedbackSchema } from '~~/src/shared/config/validation'
 
-defineRouteMeta({
-  openAPI: {
-    parameters: [{ in: 'cookie', name: 'token', required: true, schema: { type: 'string', enum: ['f', 'g'] } }],
-    requestBody: {
-      required: true,
-      content: {
+// defineRouteMeta({
+//   openAPI: {
+//     parameters: [{ in: 'cookie', name: 'token', required: true, schema: { type: 'string', enum: ['f', 'g'] } }],
+//     requestBody: {
+//       required: true,
+//       content: {
 
-        'application/json': {
-          schema: { description: 'Отправить фидбек', type: 'object', properties: { comment: { type: 'string' } } },
-        },
-      },
-    },
-  },
-})
+//         'application/json': {
+//           schema: { description: 'Отправить фидбек', type: 'object', properties: { comment: { type: 'string' } } },
+//         },
+//       },
+//     },
+//   },
+// })
 
 export default defineEventHandler(async (event) => {
   const { userId } = await authRepo.assertAuthenticated(event)
